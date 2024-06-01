2c6ea0a4ffb522cfd984420ad2a774f6b1178ba2
lema samuel <lemasam10@gmail.com>
2024-06-01 11:58:26 +0300

    completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
-
+import re
 class MyString:
-  pass
+      def __init__(self, value = ""):
+        self._value = value
+    
+      def get_value(self):
+        return self._value
+
+      def set_value(self, stringVal):
+        if (type(stringVal) == str):
+          self._value = stringVal
+        else:
+          print("The value must be a string.")
+
+      value = property(get_value, set_value)
+
+      def is_sentence(self):
+        return self._value.endswith(".")
+
+      def is_question(self):
+        return self._value.endswith("?")
+
+      def is_exclamation(self):
+        return self._value.endswith("!")
+
+      def count_sentences(self):
+        # Split the string using regular expressions to handle multiple sentence-ending punctuation
+          sentences = re.split(r'[.!?]', self._value)
+        # Filter out empty strings and return the count of non-empty sentences
+          return len([sentence for sentence in sentences if sentence.strip() != ""])
+pass
